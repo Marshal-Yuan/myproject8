e3600ef2e9da71cbf6fb428807562d9be519ac60
Marshal <ga031243@gmail.com>
2022-06-13 00:16:46 +0800

    Kotlin Hello

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,11 @@
+package com.marshal;
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin!!")
+    }
+}
+    fun main() {
+        val p = PersonKotlin()
+        p.greeting()
+    }
